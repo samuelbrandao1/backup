586547427087bf2589df5c5baadd8a29ab137384
Samuel de Araujo Brandao <samuelbaraujo19@gmail.com>
2026-07-18 01:25:16 -0300

    Adding some new packages to pacman and homebrew to insatll.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,19 @@ sudo pacman -Syu --needed - < packages/pacman.txt
 # Pacotes da AUR
 yay -Syu --needed - < packages/aur.txt
 
+# Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv zsh)"
+
 # Oh-My-Zsh installation
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# zsh-vi-mode
+git clone https://github.com/jeffreytse/zsh-vi-mode \
+  $ZSH_CUSTOM/plugins/zsh-vi-mode
