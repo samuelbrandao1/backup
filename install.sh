b3f1109ad59609ae5c273b940919727791ceaafe
Samuel de Araujo Brandao <samuelbaraujo19@gmail.com>
2026-07-18 00:43:19 -0300

    Adding oh-my-zsh installation to install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,6 @@ sudo pacman -Syu --needed - < packages/pacman.txt
 
 # Pacotes da AUR
 yay -Syu --needed - < packages/aur.txt
+
+# Oh-My-Zsh installation
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
