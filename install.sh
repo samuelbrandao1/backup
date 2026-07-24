82f5ce2d8c64d89d71c1061a0a31b66d57715b9c
Samuel de Araujo Brandao <samuelbaraujo19@gmail.com>
2026-07-24 09:42:25 -0300

    Modifying install.sh according to GPT 😭

--- install.sh
+++ install.sh
@@ -1,27 +1,63 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Pacotes oficiais
+set -e
+
+echo "==> Installing official packages..."
 sudo pacman -Syu --needed - < packages/pacman.txt
 
-# Pacotes da AUR
+echo "==> Installing AUR packages..."
 yay -Syu --needed - < packages/aur.txt
 
-# Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv zsh)"
+echo "==> Installing Homebrew..."
+if ! command -v brew >/dev/null 2>&1; then
+    NONINTERACTIVE=1 /bin/bash -c \
+        "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
 
-# Oh-My-Zsh installation
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Carrega o brew para esta sessão
+if [ -f /home/linuxbrew/.linuxbrew/bin/brew ]; then
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
 
-# zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+echo "==> Installing Oh My Zsh..."
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    RUNZSH=no CHSH=no KEEP_ZSHRC=yes sh -c \
+        "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
 
-# zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
+PLUGIN_DIR="$ZSH_CUSTOM/plugins"
 
-# zsh-vi-mode
-git clone https://github.com/jeffreytse/zsh-vi-mode \
-  $ZSH_CUSTOM/plugins/zsh-vi-mode
+mkdir -p "$PLUGIN_DIR"
 
-# Atuin
-curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
+echo "==> Installing zsh-autosuggestions..."
+if [ ! -d "$PLUGIN_DIR/zsh-autosuggestions" ]; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions \
+        "$PLUGIN_DIR/zsh-autosuggestions"
+fi
+
+echo "==> Installing zsh-syntax-highlighting..."
+if [ ! -d "$PLUGIN_DIR/zsh-syntax-highlighting" ]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting \
+        "$PLUGIN_DIR/zsh-syntax-highlighting"
+fi
+
+echo "==> Installing zsh-vi-mode..."
+if [ ! -d "$PLUGIN_DIR/zsh-vi-mode" ]; then
+    git clone https://github.com/jeffreytse/zsh-vi-mode \
+        "$PLUGIN_DIR/zsh-vi-mode"
+fi
+
+echo "==> Installing Atuin..."
+if ! command -v atuin >/dev/null 2>&1; then
+    curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
+fi
+
+echo "==> Setting Zsh as default shell..."
+if [ "$SHELL" != "$(which zsh)" ]; then
+    chsh -s "$(which zsh)"
+fi
+
+echo
+echo "Installation complete."
+echo "Log out and log back in for shell changes to take effect."
